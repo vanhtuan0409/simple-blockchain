2ce222bf11fd20a3de72cacb49b19cdacd658772
tuan <vanhtuan0409@gmail.com>
2017-06-21 16:09:04 +0900

    add url and path for 404 and commit

--- src/server.js
+++ src/server.js
@@ -1,15 +1,38 @@
 // @flow
 import express from "express";
+import parser from "body-parser";
 import BlockChain from "./blockchain";
 import Block from "./block";
 
+// Create actual blockchain data
 const bc = new BlockChain();
 
 const app = express();
+app.use(parser.json());
 
 app.get("/chain", function(req, res) {
   const chain = bc.getChain();
   res.json(chain);
+});
+
+app.post("/commit", function(req, res) {
+  const body = req.body;
+  if (!body.data) {
+    res.status(400).end();
+    return;
+  }
+  try {
+    bc.commit(body.data);
+    res.status(200).end();
+  } catch (err) {
+    res.status(500).json({
+      error: err.message
+    });
+  }
+});
+
+app.all("*", function(req, res) {
+  res.status(404).end();
 });
 
 const port = process.env.HTTP_PORT || 3000;
