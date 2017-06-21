7ac51174029bdf9e31e970e5b0eb1f09a21b1db0
tuan <vanhtuan0409@gmail.com>
2017-06-21 15:59:08 +0900

    add simple server to block chain

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
 // @flow
+import express from "express";
 import BlockChain from "./blockchain";
 import Block from "./block";
 
-const chain = new BlockChain();
-chain.commit({ msg: "new block" });
+const bc = new BlockChain();
+
+const app = express();
+
+app.get("/chain", function(req, res) {
+  const chain = bc.getChain();
+  res.json(chain);
+});
+
+const port = process.env.HTTP_PORT || 3000;
+app.listen(port, function() {
+  console.log(`Node is running on port: ${port}`);
+});
